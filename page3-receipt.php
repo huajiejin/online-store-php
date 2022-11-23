4e324757117036e63b22eb88ee6f6b56a46f03d2
ravalikrishna5121 <ravalikrishna1996@gmail.com>
2022-11-22 19:17:35 -0500

    Update page3-receipt.php

--- page3-receipt.php
+++ page3-receipt.php
@@ -16,6 +16,60 @@ $_SESSION['creditCardExpiryYear'] = $creditCardExpiryYear = empty($_SESSION['cre
 $_SESSION['email'] = $email = empty($_SESSION['email']) ? "" : $_SESSION['email'];
 $_SESSION['password'] = $password = empty($_SESSION['password']) ? "" : $_SESSION['password'];
 $_SESSION['confirmPassword'] = $confirmPassword = empty($_SESSION['confirmPassword']) ? "" : $_SESSION['confirmPassword'];
+
+$TotalBeforeTax = 0;
+$ProvinceTax = 0;
+$TaxRate = 0;
+$TotaLAfterTax = 0;
+ 
+$TotalBeforeTax = $apples * 1 + $oranges * 2;
+if ($province == "Alberta"){
+	$TaxRate = 0.05;
+}
+else if ($province == "British Columbia"){
+	$TaxRate = 0.12;
+}
+else if ($province == "Manitoba"){
+	$TaxRate = 0.12;
+}
+else if ($province == "New Brunswick"){
+	$TaxRate = 0.15;
+}
+else if ($province == "Newfoundland and Labrador"){
+	$TaxRate = 0.15;
+}
+else if ($province == "Northwest Territories"){
+	$TaxRate = 0.05;
+}
+else if ($province == "Nova Scotia"){
+	$TaxRate = 0.15;
+}
+else if ($province == "Nunavut"){
+	$TaxRate = 0.05;
+}
+else if ($province == "Ontario"){
+	$TaxRate = 0.13;
+}
+else if ($province == "Prince Edward Island"){
+	$TaxRate = 0.15;
+}
+else if ($province == "Quebec"){
+	$TaxRate = 0.14;
+}
+else if ($province == "Saskatchewan"){
+	$TaxRate = 0.11;
+}
+else if ($province == "Yukon"){
+	$TaxRate = 0.05;
+}
+
+$ProvinceTax = $TaxRate * $TotalBeforeTax;
+
+$TotaLAfterTax = $ProvinceTax + $TotalBeforeTax;
+
+if ($TotalBeforeTax <10){
+
+}
 ?>
 
 <!DOCTYPE html>
@@ -39,12 +93,12 @@ $_SESSION['confirmPassword'] = $confirmPassword = empty($_SESSION['confirmPasswo
 	<main>
 		<section class="pd-20">
 			<h3>Receipt</h3>
-			<section>
-				<!-- TODO: Calculate and dispaly the receipt -->
-				<p>Total Price Before Tax: $${totalPrice.toFixed(2)}</p>
-				<p>The province: ${province}</p>
-				<p>The tax rate: ${taxRate * 100}%</p>
-				<p>Total Price After Tax: $${totalPriceAfterTax.toFixed(2)}</p>
+			<section class="<?php if($TotalBeforeTax <10) { echo 'hidden'; }?>">
+				<!-- Calculate and dispaly the receipt -->
+				<p>Total Price Before Tax: $<?php echo $TotalBeforeTax?></p>
+				<p>The province: <?php echo $province?></p>
+				<p>The tax rate: <?php echo $TaxRate?></p>
+				<p>Total Price After Tax: $<?php echo $TotaLAfterTax?></p>
 
 				<h4>Form Data From Page 2</h4>
 				<p>name: Hello</p>
@@ -60,10 +114,12 @@ $_SESSION['confirmPassword'] = $confirmPassword = empty($_SESSION['confirmPasswo
 				<p>password: 1234</p>
 				<p>confirmPassword: 1234</p>
 			</section>
-			<!-- TODO: Display error message under proper condition -->
-			<section class="error-message">the minimum purchase should not be less than $10.</section>
+			<!-- Display error message under proper condition -->
+			<section class="error-message"><?php if($TotalBeforeTax <10) { echo "the minimum purchase should not be less than $10."; }?></section>
 		</section>
 	</main>
 	<footer class="bg-6 color-1">&copy; Copyright 2022 Jay Huajie Jin - 8819478, Ravali krishna - 8826296</footer>
 </body>
 </html>
+
+
